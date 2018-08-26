f515d934f5e5ae78558c84093b7ce005f580bc14
AsiaW-S <wszelaka.joanna@gmail.com>
2018-08-26 15:20:18 +0200

    clickWhenReady

--- src/main/java/pl/lait/selenium/App.java
+++ src/main/java/pl/lait/selenium/App.java
@@ -2,15 +2,18 @@ package pl.lait.selenium;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 /**
  * Hello world!
  *
  */
 public class App {
-	static WebDriver driver;
+	static WebDriver driver;  // globalna zmienna
 
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
@@ -21,7 +24,8 @@ public class App {
 		driver = new FirefoxDriver(cap);
 		driver.get("http://newtours.demoaut.com");
 		sleep(2);
-		driver.findElement(By.linkText("SIGN-ON")).click();
+		//driver.findElement(By.linkText("SIGN-ON")).click();
+		clickWhenReady(By.linkText("SIGN-ON"), 10);
 		sleep(3);
 		driver.findElement(By.name("userName")).sendKeys("Asiaws");
 		driver.findElement(By.name("password")).sendKeys("qwe123");
@@ -41,4 +45,13 @@ public class App {
 		}
 
 	}
+
+	public static void clickWhenReady(By locator, int timeout) {
+
+		WebElement element = null;
+		WebDriverWait wait = new WebDriverWait(driver, timeout);
+		element = wait.until(ExpectedConditions.elementToBeClickable(locator));
+		element.click();
+
+ }
 }
